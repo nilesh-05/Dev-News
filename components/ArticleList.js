160853a83d360277d85d0a936abf703ae6951aad
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-05-05 22:44:10 +0530

    used getServerSideProps

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,11 @@
 import articleStyles from "../styles/Article.module.css";
+import ArticleItem from "./ArticleItem";
 
 const ArticleList = ({ articles }) => {
 	return (
 		<div className={articleStyles.grid}>
 			{articles.map((article, id) => (
-				<h3>{article.title}</h3>
+				<ArticleItem article={article} />
 			))}
 		</div>
 	);
